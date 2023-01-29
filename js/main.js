0fd35866387368c93dd7ca59e99bff5828ef9eb7
boberace <boberace@hotmail.com>
2023-01-29 17:21:38 -0500

    changed websocket

--- js/main.js
+++ js/main.js
@@ -7,24 +7,63 @@ window.onload = () => {
   }
 }
 
-
-var connection = new WebSocket(wsServer, ['arduino']);
-connection.onopen = function () {  
-    connection.send('Connect ' + new Date()); 
-}; 
-connection.onerror = function (error) {    
-    console.log('WebSocket Error ', error);
-};
-connection.onmessage = function (e) {  
-    console.log('Server: ', e.data);
-};
-function sendRGB() {  
-    var r = parseInt(document.getElementById('r').value).toString(16);  
-    var g = parseInt(document.getElementById('g').value).toString(16);  
-    var b = parseInt(document.getElementById('b').value).toString(16);  
-    if(r.length < 2) { r = '0' + r; }   
-    if(g.length < 2) { g = '0' + g; }   
-    if(b.length < 2) { b = '0' + b; }   
-    var rgb = '#'+r+g+b;    
-    console.log('RGB: ' + rgb); connection.send(rgb); 
+var gateway = `ws://${window.location.hostname}/ws`;
+var websocket;
+function initWebSocket() {
+  console.log('Trying to open a WebSocket connection...');
+  websocket = new WebSocket(gateway);
+  websocket.onopen    = onOpen;
+  websocket.onclose   = onClose;
+  websocket.onmessage = onMessage; // <-- add this line
 }
+function onOpen(event) {
+  console.log('Connection opened');
+}
+
+function onClose(event) {
+  console.log('Connection closed');
+  setTimeout(initWebSocket, 2000);
+}
+function onMessage(event) {
+  var state;
+  if (event.data == "1"){
+    state = "ON";
+  }
+  else{
+    state = "OFF";
+  }
+  document.getElementById('state').innerHTML = state;
+}
+window.addEventListener('load', onLoad);
+function onLoad(event) {
+  initWebSocket();
+  initButton();
+}
+
+function initButton() {
+  document.getElementById('button').addEventListener('click', toggle);
+}
+function toggle(){
+  websocket.send('toggle');
+}
+
+// var connection = new WebSocket(wsServer, ['arduino']);
+// connection.onopen = function () {  
+//     connection.send('Connect ' + new Date()); 
+// }; 
+// connection.onerror = function (error) {    
+//     console.log('WebSocket Error ', error);
+// };
+// connection.onmessage = function (e) {  
+//     console.log('Server: ', e.data);
+// };
+// function sendRGB() {  
+//     var r = parseInt(document.getElementById('r').value).toString(16);  
+//     var g = parseInt(document.getElementById('g').value).toString(16);  
+//     var b = parseInt(document.getElementById('b').value).toString(16);  
+//     if(r.length < 2) { r = '0' + r; }   
+//     if(g.length < 2) { g = '0' + g; }   
+//     if(b.length < 2) { b = '0' + b; }   
+//     var rgb = '#'+r+g+b;    
+//     console.log('RGB: ' + rgb); connection.send(rgb); 
+// }

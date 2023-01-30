58a68eac0b33a7104fe1ef4274e6d16d12ab5750
boberace <boberace@hotmail.com>
2023-01-30 14:47:35 -0500

    incorporated button from websocket demo https://randomnerdtutorials.com/esp32-websocket-server-arduino/

--- js/main.js
+++ js/main.js
@@ -7,42 +7,45 @@ window.onload = () => {
   }
 }
 
-var gateway = `ws://${window.location.hostname}/ws`;
-var websocket;
-function initWebSocket() {
-  console.log('Trying to open a WebSocket connection...');
-  websocket = new WebSocket(gateway);
-  websocket.onopen    = onOpen;
-  websocket.onclose   = onClose;
-  websocket.onmessage = onMessage; // <-- add this line
-}
-function onOpen(event) {
-  console.log('Connection opened');
-}
+    var gateway = `ws://192.168.0.188:80/ws`;
+    var websocket;
+    function initWebSocket() {
+        console.log('Trying to open a WebSocket connection...');
+        websocket = new WebSocket(gateway);
+        websocket.onopen    = onOpen;
+        websocket.onclose   = onClose;
+        websocket.onmessage = onMessage; // <-- add this line
+    }
+    function onOpen(event) {
+        console.log('Connection opened');
+        websocket.send('getLedState');
+    }
 
-function onClose(event) {
-  console.log('Connection closed');
-  setTimeout(initWebSocket, 2000);
-}
-function onMessage(event) {
-  var state;
-  if (event.data == "1"){
-    state = "ON";
-  }
-  else{
-    state = "OFF";
-  }
-  document.getElementById('state').innerHTML = state;
-}
-window.addEventListener('load', onLoad);
-function onLoad(event) {
-  initWebSocket();
-  initButton();
-}
+    function onClose(event) {
+        console.log('Connection closed');
+        setTimeout(initWebSocket, 2000);
+    }
+    function onMessage(event) {
+        var state;
+        if (event.data == "1"){
+        state = "ON";
+        document.getElementById('state').innerHTML = state;
+        }
+        else if (event.data == "0"){
+        state = "OFF";
+        document.getElementById('state').innerHTML = state;
+        }
+        console.log(event.data);
+    }
+    window.addEventListener('load', onLoad);
+    function onLoad(event) {
+        initWebSocket();
+        initButton();
+    }
 
-function initButton() {
-  document.getElementById('button').addEventListener('click', toggle);
-}
-function toggle(){
-  websocket.send('toggle');
-}
+    function initButton() {
+        document.getElementById('button').addEventListener('click', toggle);
+    }
+    function toggle(){
+        websocket.send('toggle');
+    }
